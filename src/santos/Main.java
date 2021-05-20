7bb5551f00171f0876599ae310dacf0af6033479
unknown <lucas.sms@gmail.com>
2021-05-19 23:30:37 -0300

    Initial commit

---
+++ src/santos/Main.java
@@ -0,0 +1,9 @@
+package santos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String name = "Lucas";
+        System.out.println("Hello, my name is " + name);
+    }
+}
