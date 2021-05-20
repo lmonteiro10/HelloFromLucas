3165952dbcbd5ec1f0de8c40731abb20e03f09f7
Mari Lopes <marieiterer@gmail.com>
2021-05-20 09:56:49 -0300

    Line added

--- src/santos/Main.java
+++ src/santos/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         String name = "Lucas";
+        String mariName = "Mariana";
         System.out.println("Hello, my name is " + name);
+        System.out.println("Hello " + name + ", my name is " + mariName);
+
     }
 }
