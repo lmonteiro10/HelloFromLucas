17730af7ba76852a663cbad0a1d546cc0a242b2a
FerracciuO <alv20031@byui.edu>
2021-05-22 18:18:28 -0300

    Hello Lucas, I add my name on your file.

--- src/santos/Main.java
+++ src/santos/Main.java
@@ -5,8 +5,9 @@ public class Main {
     public static void main(String[] args) {
         String name = "Lucas";
         String mariName = "Mariana";
+        String oscName = "Oscar";
         System.out.println("Hello, my name is " + name);
         System.out.println("Hello " + name + ", my name is " + mariName);
-
+        System.out.println("Hello " + name + ", her name is " + mariName + " and mine is " + oscName);
     }
 }
